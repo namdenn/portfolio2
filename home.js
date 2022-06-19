461c84983e4593a2375950ae9263985ac54b92ff
namdenn <tuannam162@gmail.com>
2022-06-20 01:40:44 +0700

    first commit

---
+++ home.js
@@ -0,0 +1,11 @@
+document.getElementById("button1").addEventListener("click", ()=>{
+    window.location.href="abtme.html"
+})
+
+document.getElementById("button2").addEventListener("click", ()=>{
+    window.location.href="achive.html"
+})
+
+document.getElementById("button3").addEventListener("click", ()=>{
+    window.location.href="act.html"
+})
